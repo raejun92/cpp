56fbdbd410148161a7554b97441738eb3be3b77a
raejun92 <cvcv1324@naver.com>
2022-02-05 18:10:22 +0900

    fix: ex04 s1을 s2로 변환하는 과정 수정

--- cpp01/ex04/Replace.cpp
+++ cpp01/ex04/Replace.cpp
@@ -13,7 +13,7 @@ void Replace::change(std::ifstream& readFile, std::ofstream& writeFile) {
 		getline(readFile, tmp);
 		int findIndex = tmp.find(s1);
 		while (findIndex != -1) {
-			std::string newLine = tmp.substr(0, findIndex) + s2 + tmp.substr(findIndex + s2.length());
+			std::string newLine = tmp.substr(0, findIndex) + s2 + tmp.substr(findIndex + s1.length());
 			
 			tmp = newLine;
 			findIndex = tmp.find(s1);
