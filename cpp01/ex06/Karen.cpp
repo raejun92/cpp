1928a61312578c3c45cd8f8d4a4015b1e44a3fd9
raejun92 <cvcv1324@naver.com>
2022-02-05 17:38:11 +0900

    fix: ex06 출력문 내용 수정

--- cpp01/ex06/Karen.cpp
+++ cpp01/ex06/Karen.cpp
@@ -8,18 +8,22 @@ Karen::Karen() {
 }
 
 void Karen::debug() {
+	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n";
 }
 
 void Karen::info() {
+	std::cout << "[ INFO ]\n";
 	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n";
 }
 
 void Karen::warning() {
+	std::cout << "[ WARNING ]\n";
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.\n";
 }
 
 void Karen::error() {
+	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable, I want to speak to the manager now.\n";
 }
 
