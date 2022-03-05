7fc788a09e9b3a11e7c7317c16d013c0e54540ac
raejun92 <cvcv1324@naver.com>
2022-03-06 01:10:53 +0900

    feat: cpp07/ex00 구현

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,28 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T& first, T& second) {
+	T temp;
+	temp = first;
+	first = second;
+	second = temp;
+}
+
+template <typename T>
+T max(T first, T second) {
+	if (first > second)
+		return first;
+	return second;
+}
+
+template <typename T>
+T min(T first, T second) {
+	if (first > second)
+		return second;
+	return first;
+}
+
+#endif
