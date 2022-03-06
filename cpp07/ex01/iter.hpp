3ce92c3d319406b0f16267d18a31d92b27fff9f0
raejun92 <cvcv1324@naver.com>
2022-03-06 13:20:00 +0900

    fix: cpp07/ex01 iter 함수 포인터 매개변수 변경 및 Makefile 수정

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -4,14 +4,14 @@
 #include <iostream>
 
 template <typename T>
-void iter(T* array, int length, void (*f)(T&)) {
+void iter(T* array, int length, void (*f)(T)) {
 	for (int i = 0; i < length; i++) {
 		f(array[i]);
 	}
 }
 
 template <typename T>
-void print_iter(T& value) {
+void print_iter(T value) {
 	std::cout << value << std::endl;
 }
 
