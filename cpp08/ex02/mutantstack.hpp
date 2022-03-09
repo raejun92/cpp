92010442cfa5e992f0be50c606ad4d31e943668b
raejun92 <cvcv1324@naver.com>
2022-03-09 12:48:11 +0900

    feat: cpp08/ex02 구현

---
+++ cpp08/ex02/mutantstack.hpp
@@ -0,0 +1,52 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>{
+	public:
+		typedef typename MutantStack<T>::stack::container_type::iterator iterator;
+		typedef typename MutantStack<T>::stack::container_type::const_iterator const_iterator;
+		typedef typename MutantStack<T>::stack::container_type::reverse_iterator reverse_iterator;
+		typedef typename MutantStack<T>::stack::container_type::const_reverse_iterator const_reverse_iterator;
+		MutantStack() {}
+		~MutantStack() {}
+		MutantStack(const MutantStack &reference) {
+			*this = reference;
+		}
+		MutantStack& operator=(const MutantStack &reference) {
+			if (this == &reference) {
+				return *this;
+			}
+			MutantStack<T>::stack::operator=(reference);
+			return *this;
+		}
+		iterator begin() {
+			return this->c.begin();
+		}
+		iterator end() {
+			return this->c.end();
+		}
+		const_iterator cbegin() const {
+			return this->c.cbegin();
+		}
+		const_iterator cend() const {
+			return this->c.cend();
+		}
+		reverse_iterator rbegin() {
+			return this->c.rbegin();
+		}
+		reverse_iterator rend() {
+			return this->c.rend();
+		}
+		const_reverse_iterator crbegin() {
+			return this->c.crbegin();
+		}
+		const_reverse_iterator crend() {
+			return this->c.crend();
+		}
+};
+
+#endif
