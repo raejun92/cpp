36e7773b2e53f08efe0172b8a1694c5fdba7b201
raejun92 <cvcv1324@naver.com>
2022-03-09 13:07:09 +0900

    fix: cpp08/ex01 메인문 try catch 추가

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -2,23 +2,28 @@
 
 int main() {
 	{
-		Span sp = Span(5);
-		sp.addNumber(5);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
+		try {
+			Span sp = Span(5);
+			sp.addNumber(5);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
 
-		Span sp2(5);
-		std::vector<int>::iterator begin;
-		std::vector<int>::iterator end;
-		begin = sp.getVector().begin();
-		end = sp.getVector().end();
-		sp2.addNumber(begin, end);
-		std::cout << sp2.shortestSpan() << std::endl;
-		std::cout << sp2.longestSpan() << std::endl;
+			Span sp2(5);
+			std::vector<int>::iterator begin;
+			std::vector<int>::iterator end;
+			begin = sp.getVector().begin();
+			end = sp.getVector().end();
+			sp2.addNumber(end, begin);
+			std::cout << sp2.shortestSpan() << std::endl;
+			std::cout << sp2.longestSpan() << std::endl;
+		} catch (std::exception &e) {
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+		
 	}
 	{
 		try {
@@ -29,7 +34,7 @@ int main() {
 			}
 			std::cout << s.shortestSpan() << std::endl;
 			std::cout << s.longestSpan() << std::endl;
-			// s.addNumber(rand());
+			s.addNumber(rand());
 		} catch (std::exception &e) {
 			std::cout << "Error: " << e.what() << std::endl;
 		}
